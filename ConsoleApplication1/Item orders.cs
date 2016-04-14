b6a8e34adf7387c931a2685cba7574ff711bc226
jdomingo9 <domingojohn51@gmail.com>
2016-04-14 12:32:50 -0400

    gg

---
+++ ConsoleApplication1/Item orders.cs
@@ -0,0 +1,27 @@
+﻿
+
+namespace ConsoleApplication1
+{
+    class Item_orders
+    {
+        public static bool AskforShowtime()
+        {
+            System.Console.Write("True/False.. Are you attending the Matinee?");
+            return bool.Parse(System.Console.ReadLine());
+        }
+
+        public static int AskForPerson(string person)
+        {
+            System.Console.Write("How many " + person + " in attendance? ");
+            return int.Parse(System.Console.ReadLine());
+        }
+
+        public static int AskforInt(string item)
+        {
+            System.Console.Write("How many " + item + " would you like? ");
+            return int.Parse(System.Console.ReadLine());
+        }
+    }
+}
+
+    
